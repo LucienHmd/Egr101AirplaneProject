09684a555788c1516094fe2063ebf21374937d4d
srhomberg <99349500+srhomberg@users.noreply.github.com>
2022-02-09 11:45:57 -0500

    variables

--- Egr101AirplaneProject.m
+++ Egr101AirplaneProject.m
@@ -1,1 +1,41 @@
-% ew 
+Wpayload %Weight paylod
+N = 12 %Number of people
+Wcargo %Cargo weight 
+Wempty %Empty weight 
+Wgross %Gross weight 
+Wfuel  %Fuel weight 
+R= 3447; %Range
+L %Lift
+p_air
+CL
+Vstall
+Vlanding 
+CL_MAX
+CDi
+CL_CRUISE
+p_CRUISE
+V_CRUISE
+AR
+CDo
+fuse_diameter
+T_CRUISE
+D_CRUISE
+T_RATED
+T_SLS
+wonky_o
+SFC
+WF_CRUISE
+hr_CRUISE
+WrF
+Tupar_ratio
+Sweep_angle
+b_wing
+fuse_wid
+Croot
+Ctip
+MAC
+SVT
+AVT
+SHT
+AHT
+LT
