3b503dc9914fd65cc330d40fdd3e2a10992cc709
Lucien <llhammond9@gmail.com>
2022-02-16 17:25:01 -0500

    Wing Planform Area Calculations

--- Egr101AirplaneProject.m
+++ Egr101AirplaneProject.m
@@ -4,15 +4,20 @@ Wpayload = N * 170 + Wcargo; %Weight payload
 %Wempty; %Empty weight 
 %Wgross; %Gross weight 
 %Wfuel;  %Fuel weight 
+
 R= 3447; %Range
-%L; %Lift
-%p_air; %air density 
+p_air = 0.002377; %air density in slugs/ft^3 at Sea Level
+%S; %Wing planform Area
+
 %CL; %Coefficient of lift
-%Vstall; %Velocity Stall
-%Vlanding; %Velocity Landing 
-%CL_MAX; %type of flap
+CL_MAX = 1.8; %type of flap (Plain)
+
+L = 12802; % Runway Length
+Vstall = sqrt(L / 0.5664); %Velocity Stall
+Vlanding = Vstall * 1.1; %Velocity Landing 
+
 %CDi; %Induced drag coefficient 
-%CL_CRUISE; %
+%CL_CRUISE; 
 %p_CRUISE; %air density at cruise altitude 
 %V_CRUISE;
 %AR; %aspect ratio
@@ -40,10 +45,12 @@ R= 3447; %Range
 %AHT;
 %LT;
 
+fprintf('Iteration 1');
 Wfuel = (0.15 + 3.33*10^(-5) * (R - 1000)); % Calculate Fuel Weight
 Wgross = (Wpayload) / (1-0.5-Wfuel); % Calculate Gross Weight to be used in calculations
+Wempty = 0.5 * Wgross;
 
-
+S = (2 * Wgross) / (CL_MAX * p_air * Vstall * Vstall);
 
 %CL = L/(.5)*(p_air)*(Vstall^2); 
 %Vlanding = Vstall * 1.1; 
